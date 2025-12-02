f349e0f771433513d49ce78ce82773b01b4c5f4e
parthkanani123 <parthkanani40@gmail.com>
2025-12-02 19:31:12 +0530

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Parth Kanani on 02/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
